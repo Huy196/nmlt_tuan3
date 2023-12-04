10c7477babd24ff2dcaada85847c20d40e5496ae
doquochuy <doquochuyy66@gmail.com>
2023-12-04 08:59:27 +0700

    First commit

---
+++ Toantu.js
@@ -0,0 +1,18 @@
+let width = prompt("Hãy nhập chiều rộng =");
+let height = prompt("Hãy nhập chiều cao =");
+
+let dientich = width * height;
+
+document.write("Chiều rộng :");
+document.write("<br/>")
+document.write(width)
+document.write("<br/>")
+
+document.write("Chiều cao :");
+document.write("<br/>")
+document.write(height)
+document.write("<br/>")
+
+
+document.write("Diện tích =" + dientich);
+
---
+++ vidu1.js
@@ -0,0 +1,2 @@
+let huy = prompt("Mày nhìn cái chó j")
+alert(huy + "ịt cụ m")
